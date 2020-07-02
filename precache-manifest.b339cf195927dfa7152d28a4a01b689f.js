8bdcf8b20001bb4be12174313e1bddef392ff828
marcelo-rafael <marcelo.rafael.goncalves@hotmail.com>
2020-07-02 19:16:51 -0300

    Updates

---
+++ precache-manifest.b339cf195927dfa7152d28a4a01b689f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b698d33d1e6610c65732",
+    "url": "/react-my-freelancer-hour/static/css/main.7e9cee9d.chunk.css"
+  },
+  {
+    "revision": "b698d33d1e6610c65732",
+    "url": "/react-my-freelancer-hour/static/js/main.b698d33d.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/react-my-freelancer-hour/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "9988294100553d55cc23",
+    "url": "/react-my-freelancer-hour/static/js/runtime~main.99882941.js"
+  },
+  {
+    "revision": "7cb58857d294ac1e09b72ea9403c690a",
+    "url": "/react-my-freelancer-hour/static/media/RobotoMono-Thin.7cb58857.ttf"
+  },
+  {
+    "revision": "d7ae61ae2320f4edb6ab241203cad1d7",
+    "url": "/react-my-freelancer-hour/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae7786418a4d28c3d1f60242d3bdfd30.js"
+  "/react-my-freelancer-hour/precache-manifest.b339cf195927dfa7152d28a4a01b689f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-my-freelancer-hour/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
