e02a92d34e7ffba4e980173e96951d4add267892
marcelo-rafael <marcelo.rafael.goncalves@hotmail.com>
2020-07-02 19:05:36 -0300

    Updates

---
+++ precache-manifest.ae7786418a4d28c3d1f60242d3bdfd30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f5f0ce1160401728147",
+    "url": "/static/css/main.a1ec01fe.chunk.css"
+  },
+  {
+    "revision": "7f5f0ce1160401728147",
+    "url": "/static/js/main.7f5f0ce1.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7cb58857d294ac1e09b72ea9403c690a",
+    "url": "/static/media/RobotoMono-Thin.7cb58857.ttf"
+  },
+  {
+    "revision": "c8c12b6e01e76cb55fd19cd98a1e78ad",
+    "url": "/index.html"
+  }
+];
